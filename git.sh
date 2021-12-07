caada0e8b7a3874bbdccf1111ca1cfd7e94d09b6
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-07 23:37:54 +0200

    update extra-src

--- git.sh
+++
@@ -1,7 +0,0 @@
-echo "# GC-Scripts-Java" >> README.md
-git init
-git add ./*
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Nurul-GC/GC-Scripts-Java.git
-git push -u origin main
