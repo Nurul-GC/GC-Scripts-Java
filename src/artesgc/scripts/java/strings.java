3087abee71410489115901699ea00481a1c490b2
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-10 02:55:08 +0200

    update java scripts

--- src/artesgc/scripts/java/strings.java
+++ src/artesgc/scripts/java/strings.java
@@ -3,7 +3,6 @@ package artesgc.scripts.java;
 public class strings {
     public static void main(String[] args){
         String str = "Ola Mundo Java";
-
         System.out.println("string original -> " + str);
         System.out.println("tamanho -> " + str.length());
         System.out.println("substring de [4, 9] -> " + str.substring(4, 9));
