e8838a424356efbb454918d8d9f00ab56b34f75b
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-12 16:18:37 +0200

    update kotlin scripts

---
+++ src/artesgc/scripts/kotlin/strings.kt
@@ -0,0 +1,8 @@
+package artesgc.scripts.kotlin
+
+fun main() {
+    val str = "Ola Mundo Java"
+    println("string original -> $str")
+    println("tamanho ->  ${str.length}")
+    println("substring de [4, 9] ->  ${str.substring(4, 9)}")
+}
