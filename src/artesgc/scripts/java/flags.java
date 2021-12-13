b52c3c853b8a6476cad7f66f4bb55cfc55f73cab
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-13 02:29:39 +0200

    update java scripts

--- src/artesgc/scripts/java/flags.java
+++ src/artesgc/scripts/java/flags.java
@@ -3,12 +3,12 @@ package artesgc.scripts.java;
 public class flags {
     public static void main(String[] args){
         // inteiros
-        System.out.printf("%d-%d-%d", 7, 12, 1999);
-        System.out.printf("%,d", 12345);  // separador de milhares (1,000)
+        System.out.printf("%d-%d-%d%n", 7, 12, 1999);
+        System.out.printf("%,d%n", 12345);  // separador de milhares (1,000)
 
         // decimais
-        System.out.printf("%f", 62.3);
-        System.out.printf("%.2f", 12.4);
-        System.out.printf("%,.2f", 12345.56789);
+        System.out.printf("%f%n", 62.3);
+        System.out.printf("%.2f%n", 12.4);
+        System.out.printf("%,.2f", 12345.56789);  // separador de milhares e decimal (1,000.00)
     }
 }
